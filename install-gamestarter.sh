37e9fbabb9c592509f4fbe477101b786b9c0c171
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-03-22 23:39:40 +0100

    Update install-gamestarter.sh

--- install-gamestarter.sh
+++ install-gamestarter.sh
@@ -43,10 +43,42 @@ mkdir -p /storage/emulators/bios
 mkdir -p /storage/emulators/saves
 mkdir -p /storage/emulators/roms
 
+# instalar el addon de Retroarch
+echo 'downloading RetroArch launcher addon...' 
+wget --no-check-certificate -O /storage/downloads/script.gamestarter.retroarch.tar.gz https://github.com/bite-your-idols/script.gamestarter.retroarch/archive/0.0.1.tar.gz
+tar -xf /storage/downloads/script.gamestarter.retroarch.tar.gz -C /storage/.kodi/addons/ -xz
+
+# instalar el addon/libs de uae4arm
+echo 'downloading uae4arm launcher addon and libs...' 
+wget --no-check-certificate -O /storage/downloads/script.gamestarter.uae4arm.tar.gz https://github.com/bite-your-idols/script.gamestarter.uae4arm/archive/0.0.1.tar.gz
+tar -xf /storage/downloads/script.gamestarter.uae4arm.tar.gz -C /storage/.kodi/addons/ -xz
+
+# instalar advanced launcher, launchers.xml/symlink y caratulas -> frontend.tar.gz ?
+echo 'downloading AdvancedLauncher frontend...'
+wget --no-check-certificate -O /storage/downloads/advanced.launcher.tar.gz https://github.com/bite-your-idols/advanced-launcher/archive/2.5.8.tar.gz
+tar -xf /storage/downloads/advanced.launcher.tar.gz -C /storage/.kodi/addons/ -xz
+
+wget --no-check-certificate -O /storage/downloads/frontend.tar.gz https://github.com/bite-your-idols/gamestarter-openelec/blob/master/packages/frontend.tar.gz?raw=true
+tar -xf /storage/downloads/frontend.tar.gz -C /storage/emulators/ -xz
+
+mkdir /storage/.kodi/userdata/addon_data/plugin.program.advanced.launcher
+ln -s /storage/emulators/frontend/launchers.xml  /storage/.kodi/userdata/addon_data/plugin.program.advanced.launcher/launchers.xml
+
+
+# Lo de Gamemaker Pi 
+
+
+
 	
 #remove downloaded files
 rm /storage/downloads/retroarch.tar.gz
+rm /storage/downloads/libretro-1.tar.gz
+rm /storage/downloads/libretro-2.tar.gz
 rm /storage/downloads/uae4arm.tar.gz
+rm /storage/downloads/script.gamestarter.retroarch.tar.gz
+rm /storage/downloads/script.gamestarter.uae4arm.tar.gz
+rm /storage/downloads/advanced.launcher.tar.gz
+rm /storage/downloads/frontend.tar.gz
 rm /storage/install-gamestarter.sh
 
 # end installation
