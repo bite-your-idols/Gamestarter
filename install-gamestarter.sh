4aae7a14962bb335aa222963968107cd3a7e60f2
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-03-22 16:49:22 +0100

    Update install-gamestarter.sh

--- install-gamestarter.sh
+++ install-gamestarter.sh
@@ -1,56 +1,32 @@
 #!/bin/bash
+# Script for downloading and installing Gamestarter
 
-# setting up the menu
-cmd=(dialog --backtitle "gamestarter installation - Version " --menu "Gamestarter Installation.\nMake your choice:\n " 14 50 16)
+#Welcome message
+echo '*******************************************************************************' 
+echo 'This script will install/update Gamestarter' 
+echo 'Version 0.0.1' 
+echo '*******************************************************************************'
 
-options=(1 "Install Gamestarter"
-         2 "Install Retroarch Addon"
-         3 "WIP"
-         4 "WIP")
+# create directories
+mkdir -p /storage/emulators/scripts
+mkdir -p /storage/downloads
 
-choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+# curl -L --output /storage/downloads/uae4arm.tar.gz --get https://github.com/bite-your-idols/gamestarter-openelec/blob/master/uae4arm.tar.gz
 
-for choice in $choices
-do
-    case $choice in
-        1) 
-        
-        wget --no-check-certificate -w 4 -O /storage/downloads/ https://github.com/bite-your-idols/gamestarter-openelec/blob/master/uae4arm.tar.gz
-        tar -xf /storage/downloads/uae4arm.tar.gz -C /storage/emulators/
-        
-	# create directories
-        #    mkdir -p /storage/emulators/scripts
+wget --no-check-certificate -O /storage/downloads/uae4arm.tar.gz http://www.dropbox.com/s/jmu7gis9ckty4gb/RetroArch_base.tar.gz?dl=0
+tar -xf /storage/downloads/uae4arm.tar.gz -C /storage/emulators/ -xz
 
-	# download the retrosmc scripts and files
-           wget --no-check-certificate -w 4 -O /storage/emulators/scripts/gamestarter.sh https://github.com/bite-your-idols/gamestarter-openelec/blob/master/scripts/gamestarter.sh
-           wget --no-check-certificate -w 4 -O /storage/emulators/scripts/gamestarter.start https://github.com/bite-your-idols/gamestarter-openelec/blob/master/scripts/gamestarter.start
-           chmod a+x /storage/emulators/scripts/gamestarter.sh
-           chmod a+x /storage/emulators/scripts/gamestarter.start
-        #  chmod a+x /storage/emulators/retroarch/retroarch
-           chmod a+x /storage/emulators/uae4arm/uae4arm
-            
 
-	# end installation
-            dialog --title "FINISHED!" --msgbox "\ndone!\nPress OK to return to the menu.\n" 11 70
 
-	# restart script
-            exec /storage/install-gamestarter.sh
-            ;;
-        2)
+# download the retrosmc scripts and files
+# wget --no-check-certificate -w 4 -O /storage/emulators/scripts/gamestarter.sh https://github.com/bite-your-idols/gamestarter-openelec/blob/master/scripts/gamestarter.sh
+# wget --no-check-certificate -w 4 -O /storage/emulators/scripts/gamestarter.start https://github.com/bite-your-idols/gamestarter-openelec/blob/master/scripts/gamestarter.start
+# chmod a+x /storage/emulators/scripts/gamestarter.sh
+# chmod a+x /storage/emulators/scripts/gamestarter.start
+# chmod a+x /storage/emulators/retroarch/retroarch
+# chmod a+x /storage/emulators/uae4arm/uae4arm
+	
 
-	# restart script
-            exec /storage/install-gamestarter.sh
-            ;;
-        3)
 
-	# restart script
-            exec /storage/install-gamestarter.sh
-            ;;
-        4)
-        
-	# restart script
-            exec /storage/install-gamestarter.sh
-            ;;
-
-    esac
-done
+# end installation
+echo 'done!'
