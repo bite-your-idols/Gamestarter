ae8b018ab18f395625a7055456b7719ea05024c6
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-22 09:22:34 +0200

    Update update.sh

--- resources/bin/update.sh
+++ resources/bin/update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Script for checking for updates 
 
+ADDON_VERSION=$1
+echo $ADDON_VERSION
+
 rm /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh
 kodi-send --action=Notification"(Gamestarter,Gamestarter is up to date,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
