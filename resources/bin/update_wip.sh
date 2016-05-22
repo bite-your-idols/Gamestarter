0c7ac2264f7ac3903fc5cbdcef1429066c67e8b5
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-22 11:40:43 +0200

    Add files via upload

---
+++ resources/bin/update_wip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script for checking for updates 
+# kodi-send --action=Notification"(Gamestarter,Checking for updates...,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+ADDON_VERSION=$1
+LATEST_VERSION="2.0.1"
+# echo $ADDON_VERSION
+# kodi-send --action=Notification"(Gamestarter,$ADDON_VERSION,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+
+
+if [ $ADDON_VERSION == $LATEST_VERSION ]
+then
+   kodi-send --action=Notification"(Gamestarter,Gamestarter is up to date,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+else
+    #do_something_else
+    kodi-send --action=Notification"(Gamestarter,There is a new version you can download,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+fi
+
+# comprobar que changelog de internet y local son iguales
+# file1=
+# file2=
+# if cmp -s "$file1" "$file2"
+# then
+#    echo "The files match"
+# else
+#    echo "The files are different"
+# fi
+
+# touch /storage/.kodi/addons/script.gamestarter/resources/bin/updated
+#rm /storage/.kodi/addons/script.gamestarter/resources/bin/updated
+rm /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh
+
