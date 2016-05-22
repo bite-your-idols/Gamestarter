174e3d9ce69042a38226b616c6eb6ebec0e51b95
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-22 09:55:29 +0200

    Update update.sh

--- resources/bin/update.sh
+++ resources/bin/update.sh
@@ -5,5 +5,7 @@ ADDON_VERSION=$1
 echo $ADDON_VERSION
 kodi-send --action=Notification"(Gamestarter,$ADDON_VERSION,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 
+touch /storage/.kodi/addons/script.gamestarter/resources/bin/updated
+#rm /storage/.kodi/addons/script.gamestarter/resources/bin/updated
 rm /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh
 # kodi-send --action=Notification"(Gamestarter,Gamestarter is up to date,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
