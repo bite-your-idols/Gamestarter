3b616b2e71b5729ecf57c4c820a2e4a49113f700
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-22 08:27:59 +0200

    Add files via upload

--- resources/bin/update.py
+++ resources/bin/update.py
@@ -14,7 +14,7 @@ script_file = os.path.realpath(__file__)
 directory = os.path.dirname(script_file)
 
 
-os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh")
-
+# os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh")
+os.system("wget --no-check-certificate -O /storage/update-gamestarter.sh https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/resources/bin/update.sh && sh /storage/update-gamestarter.sh")
 xbmcgui.Dialog().ok(addonname, "Gamestarter Updated.")
 
