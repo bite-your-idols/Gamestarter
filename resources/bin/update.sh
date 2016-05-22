9df1d36869eb793e8b19c6f8cd334438f364c267
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-22 09:09:00 +0200

    Add files via upload

--- resources/bin/update.sh
+++ resources/bin/update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Script for checking for updates
+# Script for IARL addon installation 
 
-# ADDON_VERSION=$1
+# kodi-send --action=Notification"(Gamestarter,Installing IARL,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+# wget --no-check-certificate -O /storage/iarl.tar.gz https://github.com/zach-morris/plugin.program.iarl/archive/master.tar.gz
+# tar -xf /storage/iarl.tar.gz -C /storage/.kodi/addons/ -xz
+
+# rm /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh
+# wget --no-check-certificate -O /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh https://raw.githubusercontent.com/bite-your-idols/gamestarter-openelec/master/scripts/iarl/romlaunch_OE_RPi2.sh
+# chmod a+x /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh
 
 rm /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh
-kodi-send --action=Notification"(Gamestarter,IARL addon Installed,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+kodi-send --action=Notification"(Gamestarter,Gamestarter is up to date,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
