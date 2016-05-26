cfc5e31aadd80df38d92825b49857975152daae6
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-26 22:49:44 +0200

    Add files via upload

--- resources/bin/update.py
+++ resources/bin/update.py
@@ -4,28 +4,28 @@ import os
 import os.path
 import xbmc
 import xbmcgui
-import xbmcaddon
+# import xbmcaddon
 
 # addon       = xbmcaddon.Addon()
 # addonversion= addon.getAddonInfo('version')
 
 
-addonID="script.gamestarter"
-addon       = xbmcaddon.Addon(id=addonID)
-addonversion= addon.getAddonInfo('version')
+# addonID="script.gamestarter"
+# addon       = xbmcaddon.Addon(id=addonID)
+# addonversion= addon.getAddonInfo('version')
 # addonversion="2.0.1"
 
 #xbmcgui.Dialog().ok("Gamestarter", addonversion)
 
-#xbmcgui.Dialog().ok("Gamestarter", "Checking for updates, please wait.")
+# xbmcgui.Dialog().ok("Gamestarter", "Checking for updates, please wait.")
 
 
 # script_file = os.path.realpath(__file__)
 # directory = os.path.dirname(script_file)
 
 
-# os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh")
-os.system("wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/resources/bin/update.sh && sh /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh "+addonversion)
+os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh")
+# os.system("wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/resources/bin/update.sh && sh /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh")
 # xbmcgui.Dialog().ok("Gamestarter", "Gamestarter is up to date.")
 
 
