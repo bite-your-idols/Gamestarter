b04a26329dd2c29bd17ba9b79b4ba285b1b21209
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-01 12:15:42 +0200

    Update install_emulationstation.py

--- resources/bin/install_emulationstation.py
+++ resources/bin/install_emulationstation.py
@@ -2,7 +2,7 @@ import os
 import os.path
 import xbmcgui
 
-xbmcgui.Dialog().ok("Gamestarter", "Downloading IARL, please do not power off or reboot your Raspberry Pi.")
+xbmcgui.Dialog().ok("Gamestarter", "Installing EmulationStation, please do not power off or reboot your Raspberry Pi.")
 
 
 script_file = os.path.realpath(__file__)
@@ -11,6 +11,6 @@ directory = os.path.dirname(script_file)
 # xbmcgui.Dialog().ok("Gamestarter", "Done!")
 # os.system("sh "+directory+"/resources/bin/install_iarl.sh")
 
-os.system("sh  https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/assets/install_iarl.sh")
+os.system("sh  https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/assets/install_emulationstation.sh")
 
-xbmcgui.Dialog().ok("Gamestarter", "IARL addon downloaded, please install from zip located in Storage folder")
+xbmcgui.Dialog().ok("Gamestarter", "EmulationStation installed, please reboot your system.")
