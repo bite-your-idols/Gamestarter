1685995ce871b5ce97851680dc811dadf2e1c0fa
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-01 11:38:18 +0200

    Update install_gamemaker.py

--- resources/bin/install_gamemaker.py
+++ resources/bin/install_gamemaker.py
@@ -14,6 +14,7 @@ directory = os.path.dirname(script_file)
 # xbmcgui.Dialog().ok("Gamestarter", "Done!")
 # os.system("sh "+directory+"/resources/bin/install_iarl.sh")
 
-os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/install_gamemaker.sh")
+# os.system("sh  /storage/.kodi/addons/script.gamestarter/resources/bin/install_gamemaker.sh")
+os.system("sh  https://github.com/bite-your-idols/gamemaker-pi/raw/master/gamemaker-gamestarter.sh")
 
 xbmcgui.Dialog().ok("Gamestarter", "GameMaker Pi ports installed, please reboot your Raspberry Pi.")
