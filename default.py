2f2d349ce529fb282437095702fa3704cf23a90e
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-07 16:51:51 +0200

    Update default.py

--- default.py
+++ default.py
@@ -13,7 +13,8 @@ directory = os.path.dirname(script_file)
 # primero habria que comprobar si es la priemra vez que se lanza entonces hacer la instalacion:
 if os.path.isfile(directory+"/resources/bin/installed") == False: 
  	xbmcgui.Dialog().ok(addonname, "This is the first time you run Gamestarter. We are going to make a first-time-setup, please do not switch off your Raspberry Pi until process is finished.")
- 	os.system("sh "+directory+"/resources/bin/install.sh "+directory)
+ 	# os.system("sh "+directory+"/resources/bin/install.sh "+directory)
+ 	os.system("sh "+directory+"/resources/bin/install.sh "+directory+" &> /storage/.config/gamestarter.log")
  	xbmcgui.Dialog().ok(addonname, "Done. Copy your roms & system bios and enjoy!")
 
 # las siguientes veces directamente lanzariamos RetroArch o Emulationstation
