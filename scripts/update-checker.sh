cf09845fd1003952083f4ade46b66c1833190e11
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-09 11:13:49 +0200

    Update update-checker.sh

--- scripts/update-checker.sh
+++ scripts/update-checker.sh
@@ -3,7 +3,7 @@
 kodi-send --action=Notification"(Gamestarter,Checking for updates...,4000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 
 # comprobar que changelog de internet y local son iguales
-wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/changelog_latest.txt https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/changelog.txt
+wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/changelog_latest.txt https://github.com/bite-your-idols/Gamestarter-Pi/blob/master/script.gamestarter/changelog.txt
 VERSION_LOCAL=$(head -c 6 /storage/.kodi/addons/script.gamestarter/changelog.txt) 
 VERSION_ONLINE=$(head -c 6 /storage/.kodi/addons/script.gamestarter/changelog_latest.txt) 
 # echo $VERSION_LOCAL
