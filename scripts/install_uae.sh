0f176685000fcc427d292b33fb6af73fffacb530
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-10 10:27:25 +0200

    uae4arm as an optional installation

---
+++ scripts/install_uae.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script for UAE4ARM emulator installation 
+
+# kodi-send --action=Notification"(Gamestarter,Installing IARL,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
+ADDON_DIRECTORY="/storage/.kodi/addons/script.gamestarter"
+CONFIG_DIRECTORY="/storage/.config"
+
+wget --no-check-certificate -O /storage/uae4arm.tar.gz https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/packages/uae4arm.tar.gz
+tar -xf /storage/uae4arm.tar.gz -C /storage/uae4arm-temp/ -xz
+rm /storage/emulationstation.tar.gz
+
+mv /storage/uae4arm-temp/uae4arm $ADDON_DIRECTORY/resources/bin/uae4arm
+mv /storage/uae4arm-temp/lib/* /storage/.kodi/addons/script.gamestarter/lib/
+rm -rf /storage/uae4arm-temp/
+
+chmod a+x $ADDON_DIRECTORY/resources/bin/uae4arm
+
+# kodi-send --action=Notification"(Gamestarter,IARL addon downloaded,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
