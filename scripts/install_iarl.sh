440671c59dc4eda17784035eb4325a504373fe3b
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-10 10:40:22 +0200

    v1.5.5

--- scripts/install_iarl.sh
+++ scripts/install_iarl.sh
@@ -4,9 +4,9 @@
 # kodi-send --action=Notification"(Gamestarter,Installing IARL,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 # wget --no-check-certificate -O /storage/plugin.program.iarl.zip https://github.com/zach-morris/plugin.program.iarl/archive/master.zip
 # lo cambiamos por el de la repo de zACHMORRIS
-wget --no-check-certificate -O /storage/plugin.program.iarl.zip https://github.com/zach-morris/repository.zachmorris/raw/master/plugin.program.iarl/plugin.program.iarl-1.5.2.zip
+wget --no-check-certificate -O /storage/plugin.program.iarl.zip https://github.com/zach-morris/repository.zachmorris/raw/master/plugin.program.iarl/plugin.program.iarl-1.5.5.zip
 # y descargamos su repo para los autoupdates
-wget --no-check-certificate -O /storage/repository.zachmorris.zip https://github.com/zach-morris/repository.zachmorris/blob/master/repository.zachmorris/repository.zachmorris-1.0.0.zip
+wget --no-check-certificate -O /storage/repository.zachmorris.zip https://github.com/zach-morris/repository.zachmorris/raw/master/repository.zachmorris/repository.zachmorris-1.0.0.zip
 # tar -xf /storage/iarl.tar.gz -C /storage/.kodi/addons/ -xz
 
 # rm /storage/.kodi/addons/plugin.program.iarl-master/resources/bin/romlaunch_OE_RPi2.sh
