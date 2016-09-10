7572f75767cb5cb08919f76afb82b577cd0f6343
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-10 16:20:03 +0200

    Update install_uae.sh

--- scripts/install_uae.sh
+++ scripts/install_uae.sh
@@ -3,15 +3,14 @@
 
 # kodi-send --action=Notification"(Gamestarter,Installing IARL,2000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 ADDON_DIRECTORY="/storage/.kodi/addons/script.gamestarter"
-CONFIG_DIRECTORY="/storage/.config"
 
 wget --no-check-certificate -O /storage/uae4arm.tar.gz https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/packages/uae4arm.tar.gz
 tar -xf /storage/uae4arm.tar.gz -C /storage/uae4arm-temp/ -xz
-rm /storage/uae4arm.tar.gz
+# rm /storage/uae4arm.tar.gz
 
 mv /storage/uae4arm-temp/uae4arm $ADDON_DIRECTORY/resources/bin/uae4arm
 mv /storage/uae4arm-temp/lib/* /storage/.kodi/addons/script.gamestarter/lib/
-rm -rf /storage/uae4arm-temp/
+# rm -rf /storage/uae4arm-temp/
 
 chmod a+x $ADDON_DIRECTORY/resources/bin/uae4arm
 
