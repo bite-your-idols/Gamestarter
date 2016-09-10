fda640e5cb2c2949349f1cd941bdfa569da6d297
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-10 10:18:49 +0200

    uae4arm as an optional installation

---
+++ script.gamestarter/resources/bin/install_uae.py
@@ -0,0 +1,19 @@
+import os
+import os.path
+import xbmcgui
+
+xbmcgui.Dialog().ok("Gamestarter", "Installing UAE4ARM, please do not power off or reboot your Raspberry Pi.")
+
+
+script_file = os.path.realpath(__file__)
+directory = os.path.dirname(script_file)
+
+# xbmcgui.Dialog().ok("Gamestarter", "Done!")
+# os.system("sh "+directory+"/resources/bin/install_iarl.sh")
+
+#os.system("sh  https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/assets/install_es.sh")
+
+os.system("wget --no-check-certificate -O /storage/install-uae.sh https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/scripts/install_uae.sh && sh /storage/install-uae.sh")
+os.system("rm /storage/install-uae.sh")
+
+xbmcgui.Dialog().ok("Gamestarter", "UAE4ARM installed, please reboot your system.")
