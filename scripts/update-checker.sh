d36577302bf1803e0ce1d7fdd06a5798ece8ab83
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-11 15:00:51 +0200

    Update update-checker.sh

--- scripts/update-checker.sh
+++ scripts/update-checker.sh
@@ -6,20 +6,17 @@ kodi-send --action=Notification"(Gamestarter,Checking for updates...,4000,/stora
 wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/changelog_latest.txt https://github.com/bite-your-idols/Gamestarter-Pi/blob/master/script.gamestarter/changelog.txt
 VERSION_LOCAL=$(head -c 6 /storage/.kodi/addons/script.gamestarter/changelog.txt) 
 VERSION_ONLINE=$(head -c 6 /storage/.kodi/addons/script.gamestarter/changelog_latest.txt) 
-# echo $VERSION_LOCAL
-# echo $VERSION_ONLINE
+echo $VERSION_LOCAL
+echo $VERSION_ONLINE
 
 
 if [[ $VERSION_LOCAL == $VERSION_ONLINE ]]
 then
-   # echo "Gamestarter is up to date"
+   echo "Gamestarter is up to date"
    kodi-send --action=Notification"(Gamestarter,Gamestarter is up to date,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 else
-   # echo "There is a new version you can download"
+   echo "There is a new version you can download"
    kodi-send --action=Notification"(Gamestarter,There is a new version you can download,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 fi
 
-# touch /storage/.kodi/addons/script.gamestarter/resources/bin/updated
-#rm /storage/.kodi/addons/script.gamestarter/resources/bin/updated
 rm /storage/.kodi/addons/script.gamestarter/changelog_latest.txt
-# rm /storage/.kodi/addons/script.gamestarter/resources/bin/update.sh
