f65e5c1c9a4231c17306e961f048de7373d9460d
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-11 15:13:53 +0200

    Update update-checker.sh

--- scripts/update-checker.sh
+++ scripts/update-checker.sh
@@ -3,7 +3,7 @@
 kodi-send --action=Notification"(Gamestarter,Checking for updates...,4000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 
 # comprobar que changelog de internet y local son iguales
-wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/changelog_latest.txt https://github.com/bite-your-idols/Gamestarter-Pi/blob/master/script.gamestarter/changelog.txt
+wget --no-check-certificate -O /storage/.kodi/addons/script.gamestarter/changelog_latest.txt https://raw.githubusercontent.com/bite-your-idols/Gamestarter-Pi/master/script.gamestarter/changelog.txt
 VERSION_LOCAL=$(head -c 6 /storage/.kodi/addons/script.gamestarter/changelog.txt) 
 VERSION_ONLINE=$(head -c 6 /storage/.kodi/addons/script.gamestarter/changelog_latest.txt) 
 echo $VERSION_LOCAL
@@ -12,10 +12,10 @@ echo $VERSION_ONLINE
 
 if [[ $VERSION_LOCAL == $VERSION_ONLINE ]]
 then
-   echo "Gamestarter is up to date"
+   # echo "Gamestarter is up to date"
    kodi-send --action=Notification"(Gamestarter,Gamestarter is up to date,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 else
-   echo "There is a new version you can download"
+   # echo "There is a new version you can download"
    kodi-send --action=Notification"(Gamestarter,There is a new version you can download,6000,/storage/.kodi/addons/script.gamestarter/icon.png)"
 fi
 
