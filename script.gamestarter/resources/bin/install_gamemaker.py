3d760c1dcc0aa81f7f1baf0939dc10c6396db734
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-12 20:36:39 +0200

    txt

--- script.gamestarter/resources/bin/install_gamemaker.py
+++ script.gamestarter/resources/bin/install_gamemaker.py
@@ -3,7 +3,7 @@ import os.path
 
 import xbmcgui
 
-xbmcgui.Dialog().ok("Gamestarter", "Installing GameMaker Pi ports, please do not power off or reboot your Raspberry Pi.")
+xbmcgui.Dialog().ok("Gamestarter", "Installing GameMaker Pi ports, please do not power off your Pi.")
 
 script_file = os.path.realpath(__file__)
 directory = os.path.dirname(script_file)
