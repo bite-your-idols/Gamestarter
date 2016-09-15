f5947ec2ca9b7a5cf7b27456d65b6676217991a9
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-09-15 17:27:47 +0200

    compiler

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -8,10 +8,14 @@
 # crear los tar.gz de las carpetas de packages
 #################################################
 
-read -p "Do you want to make tar/zip packages (y/n)? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]];then
-	echo "Compilamos las carpetas de packages..."
+# read -p "Do you want to make tar/zip packages (y/n)? " -n 1 -r
+# echo    # (optional) move to a new line
+# if [[ ! $REPLY =~ ^[Yy]$ ]];then
+
+read -r -p "Do you want to make tar/zip packages? [y/n] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        echo "Compilamos las carpetas de packages..."
 	# hacer un pack de carpeta para cada package y resources/data: 
 	# emulationstation.tar.gz | libretro-extra-cores.tar.gz | uae4arm.tar.gz
 	cd packages && tar -zcvf emulationstation.tar.gz emulationstation/ && cd ..
@@ -24,7 +28,11 @@ if [[ ! $REPLY =~ ^[Yy]$ ]];then
 	cd packages && tar -zcvf ../script.gamestarter/resources/data/advancedemulatorlauncher.tar.gz advancedemulatorlauncher/ && cd ..
 	cd packages && tar -zcvf ../script.gamestarter/resources/data/advancedlauncher.tar.gz advancedlauncher/ && cd ..
 	cd packages && tar -zcvf ../script.gamestarter/resources/data/retroarch.tar.gz retroarch/ && cd ..
-fi
+        ;;
+    # *)
+        # do_something_else
+        # ;;
+esac
 
 
 #################################################
@@ -35,46 +43,53 @@ fi
 # ADDON_VERSION="LE8alpha"
 ADDON_VERSION=$1
 
-mkdir -p exports/script.gamestarter
-cp -R script.gamestarter exports/
+read -r -p "Do you want to make addon zip file for "$1"? [y/n] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+	
+	echo "compilamos los zips..."
+	mkdir -p exports/script.gamestarter
+	cp -R script.gamestarter exports/
 
-if [ "$ADDON_VERSION" = "OLE" ]; then
-	echo "Compilamos el addon para "$ADDON_VERSION" ..."
-	# OLE
-	# install.sh ADDON_VERSION
-	sed -i '/#versionstart/,/#versionend/s/ADDON_VERSION="XXX"/ADDON_VERSION="OLE"/' exports/script.gamestarter/resources/bin/install.sh
+	if [ "$ADDON_VERSION" = "OLE" ]; then
+		echo "Compilamos el addon para "$ADDON_VERSION" ..."
+		# OLE
+		# install.sh ADDON_VERSION
+		sed -i '/#versionstart/,/#versionend/s/ADDON_VERSION="XXX"/ADDON_VERSION="OLE"/' exports/script.gamestarter/resources/bin/install.sh
 
-	# retroarch_1.3.6(OLE)
-	mv exports/script.gamestarter/resources/bin/retroarch_1.3.6 exports/script.gamestarter/resources/bin/retroarch
-	rm exports/script.gamestarter/resources/bin/retroarch_1.3.6_libreelec8
+		# retroarch_1.3.6(OLE)
+		mv exports/script.gamestarter/resources/bin/retroarch_1.3.6 exports/script.gamestarter/resources/bin/retroarch
+		rm exports/script.gamestarter/resources/bin/retroarch_1.3.6_libreelec8
 
-	# seleccionar advanced launcher
-	rm exports/script.gamestarter/resources/data/advancedemulatorlauncher.tar.gz
+		# seleccionar advanced launcher
+		rm exports/script.gamestarter/resources/data/advancedemulatorlauncher.tar.gz
 
-	# dejar las libs necesarias: /lib/libbrcmEGL.so y /lib/libbrcmGLESv2.so para glupen64
+		# dejar las libs necesarias: /lib/libbrcmEGL.so y /lib/libbrcmGLESv2.so para glupen64
+		rm exports/script.gamestarter/lib/libs.txt
 
-else
-	echo "Compilamos el addon para "$ADDON_VERSION" ..."
-	# LE8alpha:
-	# install.sh ADDON_VERSION
-	sed -i '/#versionstart/,/#versionend/s/ADDON_VERSION="XXX"/ADDON_VERSION="LE8alpha"/' exports/script.gamestarter/resources/bin/install.sh
+	else
+		echo "Compilamos el addon para "$ADDON_VERSION" ..."
+		# LE8alpha:
+		# install.sh ADDON_VERSION
+		sed -i '/#versionstart/,/#versionend/s/ADDON_VERSION="XXX"/ADDON_VERSION="LE8alpha"/' exports/script.gamestarter/resources/bin/install.sh
 
-	# retroarch_1.3.6_libreelec8
-	mv exports/script.gamestarter/resources/bin/retroarch_1.3.6_libreelec8 exports/script.gamestarter/resources/bin/retroarch
-	rm exports/script.gamestarter/resources/bin/retroarch_1.3.6
+		# retroarch_1.3.6_libreelec8
+		mv exports/script.gamestarter/resources/bin/retroarch_1.3.6_libreelec8 exports/script.gamestarter/resources/bin/retroarch
+		rm exports/script.gamestarter/resources/bin/retroarch_1.3.6
 
-	# seleccionar advanced emulator launcher
-	rm exports/script.gamestarter/resources/data/advancedlauncher.tar.gz
+		# seleccionar advanced emulator launcher
+		rm exports/script.gamestarter/resources/data/advancedlauncher.tar.gz
 
-	# quitar las libs
-	rm -rf exports/script.gamestarter/lib
+		# quitar las libs
+		rm -rf exports/script.gamestarter/lib
 
-fi
+	fi
 
+	# crear el zip
+	cd exports && zip -r script.gamestarter-$ADDON_VERSION.zip script.gamestarter/ && cd ..
+	rm -rf exports/script.gamestarter/
 
-# crear el zip
-cd exports && zip -r script.gamestarter-$ADDON_VERSION.zip . -i script.gamestarter/ && cd ..
-rm -rf exports/script.gamestarter/
+esac
 
 
 echo "done!"
