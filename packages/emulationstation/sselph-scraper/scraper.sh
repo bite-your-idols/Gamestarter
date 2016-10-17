cad647223e054648385bdda563c5125f8f2ebcd3
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-10-17 21:39:54 +0200

    Update scraper.sh

--- packages/emulationstation/sselph-scraper/scraper.sh
+++ packages/emulationstation/sselph-scraper/scraper.sh
@@ -3,7 +3,8 @@
 # ln -s /storage/.config/emulationstation /storage/.emulationstation
 # /storage/.config/emulationstation/sselph-scraper/scraper -thumb_only -rom_dir="/storage/emulators/roms/nes" -output_file="/storage/.config/emulationstation/gamelists/nes/gamelist.xml" -workers=3  -add_not_found=true -image_dir="/storage/.config/emulationstation/artworks/nes" -image_path="/storage/.config/emulationstation/artworks/nes" && /storage/.config/emulationstation/sselph-scraper/scraper -thumb_only -rom_dir="/storage/emulators/roms/snes" -output_file="/storage/.config/emulationstation/gamelists/snes/gamelist.xml" -workers=3 -add_not_found=true -image_dir="/storage/.config/emulationstation/artworks/snes" -image_path="/storage/.config/emulationstation/artworks/snes"
 
-SCRAPER_PATH="/storage/.config/emulationstation/sselph-scraper/scraper"
+# SCRAPER_PATH="/storage/.config/emulationstation/sselph-scraper/scraper"
+SCRAPER_PATH="/storage/.kodi/addons/script.gamestarter/resources/bin/scraper"
 ROMS_PATH="/storage/emulators/roms"
 ARTS_PATH="/storage/emulators/arts"
 TIPO=$2
@@ -33,10 +34,11 @@ else
 		IMG_TYPE="b"
 		IMG_WIDTH=600
 	fi
+	
+	$SCRAPER_PATH -image_suffix=$IMG_SUFFIX -console_img=$IMG_TYPE -max_width=$IMG_WIDTH  -skip_check -extra_ext=$EXTRA_EXT -rom_dir=$ROMS_PATH/$CONSOLE -workers=3  -add_not_found=true -image_dir=$ARTS_PATH/$CONSOLE
+
 fi
 
-
-$SCRAPER_PATH -image_suffix=$IMG_SUFFIX -console_img=$IMG_TYPE -max_width=$IMG_WIDTH  -skip_check -extra_ext=$EXTRA_EXT -rom_dir=$ROMS_PATH/$CONSOLE -workers=3  -add_not_found=true -image_dir=$ARTS_PATH/$CONSOLE
 
 #thumbs
 #/storage/.config/emulationstation/sselph-scraper/scraper -image_suffix="" -console_img="b" -max_width=600 -rom_dir="/storage/emulators/roms/gb" -workers=3  -add_not_found=true -image_dir="/storage/emulators/arts_ES/gb"
