f49c6f3ab27f00f7510e640f13e423d8cdd907c2
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-10-20 21:14:02 +0200

    cores folder location change

--- packages/emulators/roms/ports/Doom.sh
+++ packages/emulators/roms/ports/Doom.sh
@@ -4,8 +4,8 @@
 # MY_DIR="/storage/emulators/roms/ports/CaveStory"
 MY_DIR="/storage/emulators/roms/ports/doom"
 RETROARCH_APP="/storage/.kodi/addons/script.gamestarter/resources/bin/retroarch"
-RETROARCH_CONFIG_FILE="/storage/.config/retroarch/retroarch.cfg"
-RETROARCH_CORE_DIR="/storage/.config/retroarch/cores"
+RETROARCH_CONFIG_FILE="/storage/.kodi/userdata/addon_data/script.gamestarter/retroarch/retroarch.cfg"
+RETROARCH_CORE_DIR="/storage/.kodi/addons/script.gamestarter/resources/bin/cores"
 
 chmod a+x $MY_DIR/*
 $RETROARCH_APP -c $RETROARCH_CONFIG_FILE -L $RETROARCH_CORE_DIR/prboom_libretro.so $MY_DIR/doom1.wad
--- packages/emulators/roms/ports/Quake.sh
+++ packages/emulators/roms/ports/Quake.sh
@@ -4,8 +4,8 @@
 # MY_DIR="/storage/emulators/roms/ports/CaveStory"
 MY_DIR="/storage/emulators/roms/ports/quake"
 RETROARCH_APP="/storage/.kodi/addons/script.gamestarter/resources/bin/retroarch"
-RETROARCH_CONFIG_FILE="/storage/.config/retroarch/retroarch.cfg"
-RETROARCH_CORE_DIR="/storage/.config/retroarch/cores"
+RETROARCH_CONFIG_FILE="/storage/.kodi/userdata/addon_data/script.gamestarter/retroarch/retroarch.cfg"
+RETROARCH_CORE_DIR="/storage/.kodi/addons/script.gamestarter/resources/bin/cores"
 
 chmod a+x $MY_DIR/*
 $RETROARCH_APP -c $RETROARCH_CONFIG_FILE -L $RETROARCH_CORE_DIR/tyrquake_libretro.so $MY_DIR/id1/pak0.pak
