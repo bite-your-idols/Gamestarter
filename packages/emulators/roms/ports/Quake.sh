2fc58eac3be82a46ae50f89bd7c41605137d7cef
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-10-20 21:16:18 +0200

    cores folder location change

--- packages/emulators/roms/ports/Quake.sh
+++ packages/emulators/roms/ports/Quake.sh
@@ -5,7 +5,7 @@
 MY_DIR="/storage/emulators/roms/ports/quake"
 RETROARCH_APP="/storage/.kodi/addons/script.gamestarter/resources/bin/retroarch"
 RETROARCH_CONFIG_FILE="/storage/.kodi/userdata/addon_data/script.gamestarter/retroarch/retroarch.cfg"
-RETROARCH_CORE_DIR="/storage/.kodi/addons/script.gamestarter/resources/bin/cores"
+RETROARCH_CORE_DIR="/storage/.kodi/addons/script.gamestarter/resources/bin/libretro-cores"
 
 chmod a+x $MY_DIR/*
 $RETROARCH_APP -c $RETROARCH_CONFIG_FILE -L $RETROARCH_CORE_DIR/tyrquake_libretro.so $MY_DIR/id1/pak0.pak
