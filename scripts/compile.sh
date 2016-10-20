a2c481b8f8ff4173735c14134326de9dd988b88f
bite-your-idols <josu@onclick.es>
2016-10-20 22:57:03 +0200

    v2.5 PRE RELEASE 20161020

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -72,7 +72,7 @@ case $response in
 		rm releases/script.gamestarter/resources/data/plugin.program.advanced.emulator.launcher.zip
 
 		# dejar las libs necesarias: /lib/libbrcmEGL.so y /lib/libbrcmGLESv2.so para glupen64
-		rm releases/script.gamestarter/lib/libs.txt
+		rm releases/script.gamestarter/lib/README.md
 
 	else
 		echo "Compilamos el addon para "$ADDON_VERSION" ..."
