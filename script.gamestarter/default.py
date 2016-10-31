274c0c77ef690b5c8f88ac7d7d21b055e787b4da
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-10-31 21:45:12 +0100

    splash screen just when launching addon

--- script.gamestarter/default.py
+++ script.gamestarter/default.py
@@ -27,13 +27,13 @@ else:
 	if askConfirmation == "true":
 		resultado = xbmcgui.Dialog().yesno("Gamestarter", "Exit Kodi and run "+frontend+"?");
 		if resultado:
-			# xbmc.executebuiltin('ShowPicture("/storage/.kodi/userdata/addon_data/script.gamestarter/frontend-assets/gamestarter_fanart.png")') 
+			xbmc.executebuiltin('ShowPicture("/storage/.kodi/userdata/addon_data/script.gamestarter/frontend-assets/gamestarter_fanart.png")') 
 			if frontend=="EmulationStation":
 				os.system(directory+"/resources/bin/gamestarter.sh emulationstation")
 			else:
 				os.system(directory+"/resources/bin/gamestarter.sh retroarch")
 	else:
-		# xbmc.executebuiltin('ShowPicture("/storage/.kodi/userdata/addon_data/script.gamestarter/frontend-assets/gamestarter_fanart.png")') 
+		xbmc.executebuiltin('ShowPicture("/storage/.kodi/userdata/addon_data/script.gamestarter/frontend-assets/gamestarter_fanart.png")') 
 		if frontend=="EmulationStation": 
 			os.system(directory+"/resources/bin/gamestarter.sh emulationstation")
 		else:
