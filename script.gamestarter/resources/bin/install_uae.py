013ac1c070dec1a0b2d00f9f8975298eba972f95
bite-your-idols <josu@onclick.es>
2017-02-03 15:52:08 +0100

    v2.7 pre-release

--- script.gamestarter/resources/bin/install_uae.py
+++ script.gamestarter/resources/bin/install_uae.py
@@ -16,4 +16,4 @@ directory = os.path.dirname(script_file)
 os.system("wget --no-check-certificate -O /storage/install_extras.sh https://github.com/bite-your-idols/Gamestarter-Pi/raw/master/scripts/install_extras.sh && sh /storage/install_extras.sh uae4arm &> /storage/.kodi/userdata/addon_data/script.gamestarter/gamestarter.log")
 os.system("rm /storage/install_extras.sh")
 
-xbmcgui.Dialog().ok("Gamestarter", "UAE4ARM installed, select as default Amiga emulator in settings, adn launch .adf games using Advanced (Emulator) Launcher or Emulationstation.")
+xbmcgui.Dialog().ok("Gamestarter", "UAE4ARM installed, select as default Amiga emulator in settings, then launch .adf games using Advanced (Emulator) Launcher or Emulationstation.")
