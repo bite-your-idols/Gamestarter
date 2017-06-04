552fb776fbd532176ecb56654c39f995a000e9c8
bite-your-idols <josu@onclick.es>
2017-06-04 22:27:10 +0200

    2.9 dev

--- scripts/install_extras.sh
+++ scripts/install_extras.sh
@@ -215,11 +215,12 @@ case $1 in
 		cp -R $ADDON_DIRECTORY/resources/bin/libretro-cores $ADDON_DIRECTORY/resources/bin/_libretro-cores
 		rm -rf $ADDON_DIRECTORY/resources/bin/libretro-cores
 		cat $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz.part.* > $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz
-		tar -xf $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz -C $ADDON_DIRECTORY/resources/bin/ -xz
-		rm $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz
 		rm $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz.part.aa
 		rm $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz.part.ab
 		rm $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz.part.ac
+
+		tar -xf $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz -C $ADDON_DIRECTORY/resources/bin/ -xz
+		rm $ADDON_DIRECTORY/resources/bin/libretro-cores.tar.gz	
 		rm -rf $ADDON_DIRECTORY/resources/bin/_libretro-cores
 
 		echo "Libretro Cores Updated."
