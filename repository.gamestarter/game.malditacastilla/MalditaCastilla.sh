0df6265d1e0e660d0b112f04e5e64f82f015bf5a
bite-your-idols <josu@onclick.es>
2017-08-24 15:10:45 +0200

    Gamemaker games not working

--- repository.gamestarter/game.malditacastilla/MalditaCastilla.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# . /etc/profile
-MY_DIR="/storage/.kodi/addons/game.malditacastilla"
-chmod a+x $MY_DIR/*
-$MY_DIR/MalditaCastilla
--- repository.gamestarter/game.malditacastilla/hacklib/autostart.sh
+++
@@ -1,2 +0,0 @@
-# Hack for third-party libraries
-[ $PPID -eq 1 -a -f /storage/.kodi/addons/script.hacklib-master/bin/hacklib ] && . /storage/.kodi/addons/script.hacklib-master/bin/hacklib
