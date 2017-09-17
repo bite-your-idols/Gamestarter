b27d5359b4affe7a174ee667f25ee05cbbd78413
bite-your-idols <josu@onclick.es>
2017-09-17 17:43:49 +0200

    RA back to AEL

--- repository.gamestarter/game.retroarch/addon.sh
+++ repository.gamestarter/game.retroarch/addon.sh
@@ -2,5 +2,17 @@
 . /etc/profile
 
 DIR="/storage/.kodi/addons/game.retroarch"
+# systemd-run $DIR/addon.start "$@" >> /storage/.kodi/temp/retroarch.log
+
+# AEL_CAT=$3
+# AEL_LAUNCHER=$4
+# AEL_ROM=$5
+# AEL="yes"
+
+# # echo "RetroArch [ADDON] :: Launching using AEL ($3 $4 $5 $6 $7)" >> /storage/.kodi/temp/retroarch.log
+# if [ ! "$AEL_CAT"="" ] && [! "$AEL_LAUNCHER"=""]; then
+# 	echo "RetroArch [ADDON] :: Launching using AEL ($3 $4 $5)" >> /storage/.kodi/temp/retroarch.log
+# # else
+# fi
 
 systemd-run $DIR/addon.start "$@" >> /storage/.kodi/temp/retroarch.log
