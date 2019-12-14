3525609aaff0cbd2bb9d652215ed99d2aef75f89
bite-your-idols <josu@onclick.es>
2019-12-14 09:42:48 -0500

    RA: 9.181.16

--- repository.gamestarter/update.sh
+++ repository.gamestarter/update.sh
@@ -2,7 +2,7 @@
 #################################################
 # ACTUALIZAR REPO
 #################################################
-# desde raiz:  e 
+# desde raiz: sh repository.gamestarter/update.sh
 ADDON_1='repository.gamestarter'
 ADDON_1_VER='3.0.0'
 ADDON_2='game.retroarch'
@@ -15,10 +15,10 @@ ADDON_5='plugin.program.advanced.emulator.launcher'
 ADDON_5_VER='0.9.7'
 ADDON_6='game.ppsspp'
 ADDON_6_VER='0.0.1'
-# ADDON_5='game.uae4arm'
-# ADDON_5_VER='0.0.1'
 ADDON_7='game.retroarch'
 ADDON_7_VER='9.178.15'
+ADDON_8='game.retroarch'
+ADDON_8_VER='9.181.16'
 
 
 echo ""
@@ -32,10 +32,11 @@ echo $ADDON_4' - '$ADDON_4_VER
 echo $ADDON_5' - '$ADDON_5_VER
 echo $ADDON_6' - '$ADDON_6_VER
 echo $ADDON_7' - '$ADDON_7_VER
+echo $ADDON_8' - '$ADDON_8_VER
 echo "-------------------------------------"
 echo ""
 
-ADDONS=./$ADDON_1/$ADDON_1-$ADDON_1_VER.zip' './$ADDON_2/$ADDON_2-$ADDON_2_VER.zip' './$ADDON_3/$ADDON_3-$ADDON_3_VER.zip' './$ADDON_4/$ADDON_4-$ADDON_4_VER.zip' './$ADDON_5/$ADDON_5-$ADDON_5_VER.zip' './$ADDON_6/$ADDON_6-$ADDON_6_VER.zip' './$ADDON_7/$ADDON_7-$ADDON_7_VER.zip
+ADDONS=./$ADDON_1/$ADDON_1-$ADDON_1_VER.zip' './$ADDON_2/$ADDON_2-$ADDON_2_VER.zip' './$ADDON_3/$ADDON_3-$ADDON_3_VER.zip' './$ADDON_4/$ADDON_4-$ADDON_4_VER.zip' './$ADDON_5/$ADDON_5-$ADDON_5_VER.zip' './$ADDON_6/$ADDON_6-$ADDON_6_VER.zip' './$ADDON_7/$ADDON_7-$ADDON_7_VER.zip' './$ADDON_8/$ADDON_8-$ADDON_8_VER.zip
 # ADDONS=./$ADDON_1/$ADDON_1-$ADDON_1_VER.zip' './$ADDON_2/$ADDON_2-$ADDON_2_VER.zip' './$ADDON_3/$ADDON_3-$ADDON_3_VER.zip' './$ADDON_4/$ADDON_4-$ADDON_4_VER.zip' './$ADDON_5/$ADDON_5-$ADDON_5_VER.zip' './$ADDON_6/$ADDON_6-$ADDON_6_VER.zip' './$ADDON_7/$ADDON_7-$ADDON_7_VER.zip
 
 # echo $ADDONS
